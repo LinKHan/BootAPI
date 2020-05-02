f0c988d3369a14e5edd8367de714a0842af5eb59
MdAfzalKhan <autoselenium374@gmail.com>
2020-05-02 12:48:31 -0400

    initial commit from backend all app config file

---
+++ febe/febe/src/main/java/ak/ngSping/febe/FebeApplication.java
@@ -0,0 +1,13 @@
+package ak.ngSping.febe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FebeApplication.class, args);
+	}
+
+}
---
+++ febe/febe/src/test/java/ak/ngSping/febe/FebeApplicationTests.java
@@ -0,0 +1,13 @@
+package ak.ngSping.febe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
